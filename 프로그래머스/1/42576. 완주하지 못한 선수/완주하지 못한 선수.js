be6cbd0231adde39182f8a90093d9198f0af25cc
seong5 <greenbi0852@gmail.com>
2025-09-25 15:07:29 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 65.10 ms, Memory: 66 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -0,0 +1,12 @@
+function solution(participant, completion) {
+    const map = new Map();
+    for (let p of participant) {
+    map.set(p, (map.get(p) || 0) + 1);
+  }
+    for (let c of completion) {
+        map.set(c, map.get(c) -1)
+    }
+    for (let [name,count] of map) {
+        if(count > 0) return name;
+    }
+}
