ec7c34020f3c9f03e0aeae03e68dde8b93a3b975
seong5 <greenbi0852@gmail.com>
2025-09-29 12:18:39 +0900

    [level 2] Title: 전화번호 목록, Time: 133.73 ms, Memory: 79.4 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
@@ -1,10 +1,9 @@
 function solution(phone_book) {
-  phone_book.sort();
-
-  for (let i = 0; i < phone_book.length - 1; i++) {
-    if (phone_book[i + 1].startsWith(phone_book[i])) {
-      return false;
+    const sorted = phone_book.sort()
+    for (let i = 0; i < phone_book.length -1; i++) {
+        if(phone_book[i + 1].startsWith(phone_book[i])) {
+            return false
+        }
     }
-  }
-  return true;
+    return true
 }
