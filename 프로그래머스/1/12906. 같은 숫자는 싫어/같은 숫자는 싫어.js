b8a72f5b2ec6888792c9785a1b98fccc7b81c08d
seong5 <greenbi0852@gmail.com>
2025-09-29 11:56:17 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 28.77 ms, Memory: 91.5 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -1,9 +1,10 @@
-function solution(arr) {
-  const stack = [];
-  for (const char of arr) {
-    if (stack.length === 0 || stack[stack.length - 1] !== char) {
-      stack.push(char);
+function solution(arr){
+    const stack = []
+    
+    for(const char of arr) {
+        if(stack.length === 0 || stack[stack.length -1] !== char) {
+            stack.push(char)
+        } 
     }
-  }
-  return stack;
+    return stack
 }
